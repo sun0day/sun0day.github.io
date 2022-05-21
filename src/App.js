79757305d55b1ccc89f8b0ea436a69fb312d95fd
fuhao.xu <fuhao.xu@yitu-inc.com>
2022-05-22 01:56:57 +0800

    chore: 打包构建

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Button } from 'antd-mobile'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Detail from './pages/detail'
 import logo from './logo.svg';
@@ -9,12 +9,12 @@ import 'lib-flexible/flexible'
 function App() {
   return (
     <div className="App">
-      <Router basename='/assets'>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/weather' element={<Detail />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
