a507c317ddd0fe988be617e27664d4568fd36519
fuhao.xu <fuhao.xu@yitu-inc.com>
2022-05-22 22:40:27 +0800

    feat: sw.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import logo from './logo.svg';
 import './App.css';
 import 'lib-flexible/flexible'
 
+function serviceRegister() {
+  const { serviceWorker } = navigator;
+  if (!serviceWorker) {
+    console.warn("your browser not support serviceWorker");
+    return;
+  }
+  window.addEventListener("load", async () => {
+    let sw = null;
+    const regisration = await serviceWorker.register("sw.js");
+    sw = regisration.installing || regisration.waiting || regisration.active;
+    sw && sw.addEventListener("statechange", (e) => {
+      const { state } = e.target;
+      console.log(state);
+    });
+  });
+}
+serviceRegister();
+
 function App() {
   return (
     <div className="App">
