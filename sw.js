c120b6e1e646cb5647d650f45078872435f417f2
fuhao.xu <fuhao.xu@yitu-inc.com>
2022-05-23 13:22:01 +0800

    feat: sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,10 @@ const putInCache = async (request, response) => {
   await cache.put(request, response);
 };
 
+const cleanCache = () => {
+  return cache.
+}
+
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   console.log('catch request', request)
   // First try to get the resource from the cache
@@ -17,12 +21,6 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   }
 
   // Next try to use the preloaded response, if it's there
-  const preloadResponse = await preloadResponsePromise;
-  if (preloadResponse) {
-    console.info('using preload response', preloadResponse);
-    putInCache(request, preloadResponse.clone());
-    return preloadResponse;
-  }
 
   // Next try to get the resource from the network
   try {
@@ -33,17 +31,12 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     putInCache(request, responseFromNetwork.clone());
     return responseFromNetwork;
   } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+      console.info('using preload response', preloadResponse);
+      putInCache(request, preloadResponse.clone());
+      return preloadResponse;
     }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
-    return new Response('Network error happened', {
-      status: 408,
-      headers: { 'Content-Type': 'text/plain' },
-    });
   }
 };
 
