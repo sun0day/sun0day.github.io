52efc78e070206b6f6cd2cee8245c3e55b35cdc7
sun0day <sun0day_500@163.com>
2022-08-09 23:48:39 +0800

    feat: height adaptive

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AutoAdaptiveView.vue')
+    },
+    {
+      path: '/height-adaptive',
+      name: 'height-adaptive',
+      component: () => import('../views/HeightAdaptiveView.vue')
     }
   ]
 })
